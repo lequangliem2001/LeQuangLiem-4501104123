56ce3fcbd1a45a53a4dccd940a0f89992a228750
Quang Liem <lequangliem2001@gmail.com>
2022-10-29 13:08:55 +0700

    first commit

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,15 @@
+
+
+const AboutMe = () => {
+    return (
+        <>
+            <div style={{width: '90%', margin: '0 auto', marginTop: '100px', marginLeft: '500px'}}>
+                <h2>Họ và tên: Lê Quang Liêm</h2>
+                <h2>Mã số sinh viên: 4501104123</h2>
+                <h2>Lớp: Công nghệ Web(2211COMP1031) </h2>
+            </div>
+        </>
+    )
+}
+
+export default AboutMe
